b0fcb5b611872a98b9ae5fecf8021ac7e0a0b8ee
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-09-02 04:04:56 +0530

    for-loop-problem

---
+++ for-loop-tasks.js
@@ -0,0 +1,53 @@
+// Problem-1
+for(let i = 1; i<=60; i++){
+    console.log('I will invest at least 6 hrs every single day for next 60 days!',i);
+}
+
+// problem-2
+
+for(let i=61; i<=100; i++){
+ if(i%2===1){
+    console.log(i);
+ }
+}
+
+// Problem-3
+for(let i=78; i<=98; i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
+
+// Problem-4
+
+// let sum=0;
+// for(let i=91; i<=129; i++){
+//    if(i%2===1){
+//     sum= sum+ i
+//    }
+// }
+// console.log(sum);
+
+// Problem-5
+
+// let sum = 0;
+// for(let i=51; i<=85; i++){
+//     if(i%2===0){
+//         sum+=i
+//     }
+// }
+// console.log(sum);
+
+// Problem-6
+
+let multi=9;
+for(let i=1; i<=10; i++){
+    console.log(`${multi} x ${i} = ${multi * i}`)
+}
+
+
+// Problem-7
+
+for(let i=81; i>=65; i--){
+    console.log(i);
+}
---
+++ for-loop.js
@@ -0,0 +1,7 @@
+
+let add = 0
+for(let i=1; i<=20; i++){
+        add+=i;
+
+}
+console.log('sum is :', add);
